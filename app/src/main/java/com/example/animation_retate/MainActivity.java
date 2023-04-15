af7947c9bc64416cbbe35872969da2fe270ac275
Asmaa Gharballil <asmaagharballil0@gmail.com>
2023-04-15 18:54:11 +0000

    rotate

---
+++ app/src/main/java/com/example/animation_retate/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.animation_retate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Animation anim;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.txt);
+        anim = AnimationUtils.loadAnimation(this,R.anim.rotate);
+
+        txt.setAnimation(anim);
+    }
+}
